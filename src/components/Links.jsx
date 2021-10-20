eee5f0978670a840dec9a53578db2abfebf6b5bd
Raghav Sharma <sharma.raghav34@gmail.com>
2021-10-20 23:35:10 +0530

    rectified .env mistake

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -10,8 +10,9 @@ const links = [
 export const Links = () => {
   return (
     <div className='flex items-center sm:justify-around justify-between mt-4'>
-      {links.map(({ url, text }) => (
+      {links.map(({ url, text }, idx) => (
         <NavLink
+          key={idx}
           to={url}
           className='m-2 mb-0'
           activeClassName='text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2'
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -18,7 +18,7 @@ export const ResultContextProvider = ({ children }) => {
       headers: {
         'x-user-agent': 'desktop',
         'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.API_KEY,
+        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
       },
     });
 
